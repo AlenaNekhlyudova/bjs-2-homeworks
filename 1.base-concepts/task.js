a35dbf5c585f8a69e4842e60117c1bf3292fe629
Alena Nekhlyudova <alenka.955@yandex.ru>
2025-10-19 22:54:06 +0300

    Итоговый вариант

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,7 +7,7 @@ function solveEquation(a, b, c) {
   if (discriminant === 0) {
     arr.push(-b / (2 * a));
   } else if (discriminant > 0) {
-    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d) ) / (2 * a));
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant) ) / (2 * a));
   } 
 
   return arr;
