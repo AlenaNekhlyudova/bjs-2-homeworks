169956c8ac934cddbf49f7e1e59949b99eff90a2
Alena Nekhlyudova <alenka.955@yandex.ru>
2025-10-19 22:29:34 +0300

    Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,17 +2,19 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = b**-4*a*c;
+  let discriminant = b ** 2 - 4 * a * c;
   
   if (discriminant === 0) {
-    arr.push(-b/(2*a));
+    arr.push(-b / (2 * a));
   } else if (discriminant > 0) {
-    arr.push((-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a));
-  }
+    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d) ) / (2 * a));
+  } 
 
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / 12;
+  let loanBody = amount - contribution;
+  let payment = loanBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
 }
