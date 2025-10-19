5e2f29260b6cc19d0d47cf622e0e5fb94772c30e
Alena Nekhlyudova <alenka.955@yandex.ru>
2025-10-19 22:02:32 +0300

    Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = b**-4*a*c;
   
+  if (discriminant === 0) {
+    arr.push(-b/(2*a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a));
+  }
+
   return arr;
 }
 
