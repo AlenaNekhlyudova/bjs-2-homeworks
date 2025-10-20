fe2dd95db0e6f270d84e35009936915374920eca
Alena Nekhlyudova <alenka.955@yandex.ru>
2025-10-20 21:31:16 +0300

    Задание 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,20 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  let avg = sum / arr.length;
+  avg = +avg.toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
